f4b0c50ceca2d3c55896d399a2c103b19fbd315b
potash <35871896+potahyox@users.noreply.github.com>
2018-11-24 21:42:27 +0900

    Create StringArray.c

---
+++ StringArray.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	char * strArr[3] = { "Simple", "String", "Array" };
+	printf("%s \n", strArr[0]);
+	printf("%s \n", strArr[1]);
+	printf("%s \n", strArr[2]);
+	return 0;
+}
