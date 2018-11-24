60e9ab586acc57e7e41304448e427bf19a144afe
potash <35871896+potahyox@users.noreply.github.com>
2018-11-24 19:57:39 +0900

    Create ArrayNameIsPointer.c

---
+++ ArrayNameIsPointer.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int arr[3] = { 15,25,35 };
+	int * ptr = &arr[0];
+
+	printf("%d %d \n", ptr[0], arr[0]);
+	printf("%d %d \n", ptr[1], arr[1]);
+	printf("%d %d \n", ptr[2], arr[2]);
+	printf("%d %d \n", *ptr, *arr);
+	return 0;
+}
