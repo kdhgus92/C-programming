430417a78613b02a68121a0c94efa0cf7f2791ed
potash <35871896+potahyox@users.noreply.github.com>
2018-11-25 16:38:38 +0900

    Create 15-3.c

---
+++ 15-3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i,num;
+	int front = 0, back = 9;
+	int arr[10];
+	printf("총 10개의 숫자 입력 \n");
+	for (i = 0; i < 10; i++) {
+		printf("입력: ");
+		scanf("%d", &num);
+		if (num % 2 != 0)
+			arr[front++] = num;
+		else
+			arr[back--] = num;
+	}
+	for (i = 0; i < 10; i++)
+		printf("%d ", arr[i]);
+	printf("\n\n");
+	return 0;
+}
