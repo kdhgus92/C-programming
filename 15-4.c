9c188174ec9aa0d7e036846ad30ca47f3022dcd3
potash <35871896+potahyox@users.noreply.github.com>
2018-11-25 17:20:00 +0900

    Update 15-4.c

--- 15-4.c
+++ 15-4.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+int StringLen(char * str);
+
 int main(void)
 {
 	int i, len;
@@ -9,14 +11,25 @@ int main(void)
 	scanf("%s", str);
 	len = strlen(str);
 
-	for (i = 0; i < strlen(str) / 2; i++)
+	for (i = 0; i < StringLen(str) / 2; i++)
 	{
-		if (str[i] != str[strlen(str)-1 - i])
-			break;
+		if (str[i] != str[StringLen(str) - 1 - i]) 
+		{
+			printf("회문이 아닙니다.\n");
+			return 0;
+		}
 	}
 
-	if (i >= strlen(str) / 2)
+	if (i >= StringLen(str) / 2)
 		printf("회문 입니다. \n");
 
 	return 0;
+}
+
+int StringLen(char * str)
+{
+	int len = 0;
+	while (str[len] != 0)
+		len++;
+	return len;
 }
