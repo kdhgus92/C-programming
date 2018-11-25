93e01ae04694731c318b267133dc6d856b229765
potash <35871896+potahyox@users.noreply.github.com>
2018-11-25 16:49:17 +0900

    Update 15-3.c

--- 15-3.c
+++ 15-3.c
@@ -5,8 +5,10 @@ int main(void)
 	int i,num;
 	int front = 0, back = 9;
 	int arr[10];
+	
 	printf("총 10개의 숫자 입력 \n");
-	for (i = 0; i < 10; i++) {
+	for (i = 0; i < 10; i++) 
+	{
 		printf("입력: ");
 		scanf("%d", &num);
 		if (num % 2 != 0)
@@ -14,8 +16,11 @@ int main(void)
 		else
 			arr[back--] = num;
 	}
+	
+	printf("배열 요소의 출력 : ");
 	for (i = 0; i < 10; i++)
 		printf("%d ", arr[i]);
 	printf("\n\n");
+	
 	return 0;
 }
